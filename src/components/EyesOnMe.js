959707650ceea160c52d1349d191558c65e590fe
Tevin <georgetevin0@gmail.com>
2024-09-04 23:35:15 +0300

    test

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+    const handleFocus = () =>{
+        console.log('Good!')
+    };
+
+    const handelBlur = () =>{
+        console.log('Hey! Eyes on me!')
+    };
+
+    
+    return(
+        <div>
+            <button 
+            onFocus={handleFocus}
+            onBlur={handelBlur}
+             >'Eyes on me'</button>
+        </div>
+    )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,12 @@
-// Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+    const handleChange = () => {
+        console.log('Entering password...');
     return (
-        <div></div>
+        <div>
+           <input type="password" onChange={handleChange} placeholder="Enter password"/>
+        </div>
     )
 }
 
